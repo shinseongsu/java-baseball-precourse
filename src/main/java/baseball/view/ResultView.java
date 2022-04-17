249ee69d304f5c07f4c0fd00225dc4b3b2d484a6
Shin seongsu <tlstjdtn321@naver.com>
2022-04-17 13:25:41 +0900

    fix: 한줄로 출력는 점 수정

--- src/main/java/baseball/view/ResultView.java
+++ src/main/java/baseball/view/ResultView.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class ResultView {
 
     public static void answerResult(String answer) {
-        System.out.printf("%s", answer);
+        System.out.printf("%s\n", answer);
     }
 
 }
